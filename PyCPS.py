94c046486b7afff76a80e839e0b9d1e33fec8ba9
SmaamX <90418723+SmaamX@users.noreply.github.com>
2023-06-07 20:27:23 +0330

    Add files via upload
    def grand!

--- PyCPS.py
+++ PyCPS.py
@@ -1,4 +1,4 @@
-#AutoClicker (unrecognizable)
+#AUTO and BYPASS
 try:
     import pyautogui
 except ModuleNotFoundError:
@@ -11,21 +11,32 @@ from time import sleep as sl
 from random import randint as ri
 print('By SmaamX')
 pyautogui.PAUSE = 0
+
+def grandom(x,y,z,a):
+    d = ri(1,2)
+    if d == 1:
+        r1 = ri(x,y)
+        ri2 = ri(z,a)
+        r = ri(r1,ri2)
+    else:
+        r1 = ri(z,a)
+        ri2 = ri(x,y)
+        r = ri(ri2,r1)
+    return r
+
 try:
     while True:
-        while ip('v'):
-            cd = ri(4,13)
-            if cd == 4:
-                df = ri(1,4)
-                df = df * 10 ** -1
-                sl(df)
-            r1 = ri(22,91)
-            ri2 = ri(89,192)
-            r = ri(r1,ri2)
-            r = r * 10 ** -3
-            sl(r)
+        while ip('v'): #Your hotkey for start
+            p = ri(1,20)
+            if p == 1:
+                coold = grandom(2,3,4,5)
+                coold = coold * 10 ** -1
+                sl(coold)
+            a = grandom(23,91,92,192)
+            a = a * 10 ** -3
+            sl(a)
             pyautogui.leftClick()
-        if ip('y'):
+        if ip('y'): #Your hotkey for stop
             print ('exit')
             break
 except KeyboardInterrupt:

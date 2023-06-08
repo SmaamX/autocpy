21dc89fb2f425751329af56904e468ec85219f35
SmaamX <90418723+SmaamX@users.noreply.github.com>
2023-06-08 08:31:18 +0330

    Add files via upload
    Fix keyboard

--- PyCPS.py
+++ PyCPS.py
@@ -1,9 +1,10 @@
-#AUTO and BYPASS
+#PyCPS Beta
 try:
     import pyautogui
 except ModuleNotFoundError:
     print ('pip install pyautogui')
 try:
+    import keyboard
     from keyboard import is_pressed as ip
 except ModuleNotFoundError:
     print ('pip install keyboard')
@@ -26,27 +27,31 @@ def grandom(x,y,z,a):
 
 try:
     h = 0
-    while True:
-        while ip('v'): #Your hotkey for start
-            p = ri(1,22)
-            if p == 1:
-                coold = grandom(2,3,4,5)
-                coold = coold * 10 ** -1
-                sl(coold)
-            a = grandom(21,90,91,189)
-            g = grandom(12,29,30,101)
-            if a > h and not a-g <= 0:
-                a = a-g
-            elif a-g <= 0:
-                a = g-a
-            else:
-                a = a+g
-            h = a
-            a = a * 10 ** -3
-            sl(a)
-            pyautogui.leftClick()
-        if ip('y'): #Your hotkey for stop
-            print ('Stop')
-            break
+    def start():
+        global h
+        p = ri(1,22)
+        if p == 1:
+            coold = grandom(2,3,4,5)
+            coold = coold * 10 ** -1
+            sl(coold)
+        a = grandom(21,90,91,189)
+        g = grandom(12,29,30,101)
+        if a > h and not a-g <= 0:
+            a = a-g
+        elif a-g <= 0:
+            a = g-a
+        else: a = a+g
+        h = a
+        a = a * 10 ** -3
+        sl(a)
+        pyautogui.leftClick()
+    def on_start_pressed():
+        start()
+    def on_stop_pressed():
+        print('Stop')
+        exit()
+    keyboard.add_hotkey('v', on_start_pressed)  
+    keyboard.add_hotkey('y', on_stop_pressed)
+    keyboard.wait()
 except KeyboardInterrupt:
     print ('Force stop')

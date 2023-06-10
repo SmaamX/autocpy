7582ce6e25798273e4232e5e726aa06b7cf65f14
SmaamX <90418723+SmaamX@users.noreply.github.com>
2023-06-10 15:50:45 +0330

    Add files via upload

--- PyCPS.py
+++ PyCPS.py
@@ -33,6 +33,7 @@ def grandom(x,y,z,a):
 
 try:
     h = 0
+    h2 = 0
     def start():
         global h
         p = ri(1,40)
@@ -74,87 +75,132 @@ try:
         a = a * 10 ** -3
         sl(a)
         pyautogui.leftClick()
+    def start2():
+        global h2
+        p = ri(1,40)
+        if p == 1:
+            coold = grandom(1,2,3,4)
+            coold = coold * 10 ** -1
+            sl(coold)
+        elif p == 20:
+            coold = grandom(1,3,4,5)
+            coold = coold * 10 ** -2
+            sl(coold)
+        elif p == 25:
+            coold = grandom(1,3,4,5)
+            coold = coold * 10 ** -3
+            sl(coold)
+        elif p == 30:
+            coold = grandom(1,2,3,4)
+            coold = coold * 10 ** -2
+            sl(coold)
+        elif p == 35:
+            coold = grandom(1,2,3,4)
+            coold = coold * 10 ** -3
+            sl(coold)
+        elif p == 40:
+            coold = grandom(1,3,4,5)
+            coold = coold * 10 ** -1
+            sl(coold)
+        a = grandom(11,20,21,120)
+        g = grandom(12,24,25,91)
+        m = grandom(2,5,6,8)
+        m1 = grandom(1,2,3,4)
+        m2 = grandom(2,3,4,5)
+        if a > h2 and not a/m2-g+m-m1 <= 0:
+            a = a/m2-g+m-m1
+        elif a-g <= 0:
+            a = (g-a+m)*(m1/m2)+m2
+        else: a = (a+g-m)*(m1/m2)+m2
+        h2 = a
+        a = a * 10 ** -3
+        sl(a)
+        pyautogui.rightClick()
     def on_start_pressed():
         start()
+    def on_start2_pressed():
+        start2()
     def on_stop_pressed():
         print('Stop')
         exit()
 
-    def fixkey (x,y):
-        keyboard.add_hotkey(x +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ x, on_start_pressed)
+    def fixkey (x,y,rt):
+        keyboard.add_hotkey(x +'+'+ y, rt)
+        keyboard.add_hotkey(y +'+'+ x, rt)
 
-    def fixkey3 (x,y,z):
-        keyboard.add_hotkey(x +'+'+ y +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(z+'+'+ x +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ z +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ x +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ y +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ z +'+'+ x, on_start_pressed)
+    def fixkey3 (x,y,z,rt):
+        keyboard.add_hotkey(x +'+'+ y +'+'+ z, rt)
+        keyboard.add_hotkey(z+'+'+ x +'+'+ y, rt)
+        keyboard.add_hotkey(x +'+'+ z +'+'+ y, rt)
+        keyboard.add_hotkey(y +'+'+ x +'+'+ z, rt)
+        keyboard.add_hotkey(z +'+'+ y +'+'+ x, rt)
+        keyboard.add_hotkey(y +'+'+ z +'+'+ x, rt)
 
-    def fixkey4 (x,y,z,s):
-        keyboard.add_hotkey(x +'+'+ y +'+'+ z +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ y +'+'+ s +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ s +'+'+ y +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ x +'+'+ y +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ x +'+'+ y +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ x +'+'+ s +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ s +'+'+ x +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ z +'+'+ x +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ z +'+'+ y +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ z +'+'+ s +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ s +'+'+ z +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ x +'+'+ z +'+'+ y, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ x +'+'+ z +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ x +'+'+ s +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ s +'+'+ x +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ y +'+'+ x +'+'+ z, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ y +'+'+ x +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ y +'+'+ s +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(z +'+'+ s +'+'+ y +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ z +'+'+ y +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ z +'+'+ x +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ z +'+'+ s +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(y +'+'+ s +'+'+ z +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ y +'+'+ z +'+'+ x, on_start_pressed)
+    def fixkey4 (x,y,z,s,rt):
+        keyboard.add_hotkey(x +'+'+ y +'+'+ z +'+'+ s, rt)
+        keyboard.add_hotkey(x +'+'+ y +'+'+ s +'+'+ z, rt)
+        keyboard.add_hotkey(x +'+'+ s +'+'+ y +'+'+ z, rt)
+        keyboard.add_hotkey(s +'+'+ x +'+'+ y +'+'+ z, rt)
+        keyboard.add_hotkey(z +'+'+ x +'+'+ y +'+'+ s, rt)
+        keyboard.add_hotkey(z +'+'+ x +'+'+ s +'+'+ y, rt)
+        keyboard.add_hotkey(z +'+'+ s +'+'+ x +'+'+ y, rt)
+        keyboard.add_hotkey(s +'+'+ z +'+'+ x +'+'+ y, rt)
+        keyboard.add_hotkey(x +'+'+ z +'+'+ y +'+'+ s, rt)
+        keyboard.add_hotkey(x +'+'+ z +'+'+ s +'+'+ y, rt)
+        keyboard.add_hotkey(x +'+'+ s +'+'+ z +'+'+ y, rt)
+        keyboard.add_hotkey(s +'+'+ x +'+'+ z +'+'+ y, rt)
+        keyboard.add_hotkey(y +'+'+ x +'+'+ z +'+'+ s, rt)
+        keyboard.add_hotkey(y +'+'+ x +'+'+ s +'+'+ z, rt)
+        keyboard.add_hotkey(y +'+'+ s +'+'+ x +'+'+ z, rt)
+        keyboard.add_hotkey(s +'+'+ y +'+'+ x +'+'+ z, rt)
+        keyboard.add_hotkey(z +'+'+ y +'+'+ x +'+'+ s, rt)
+        keyboard.add_hotkey(z +'+'+ y +'+'+ s +'+'+ x, rt)
+        keyboard.add_hotkey(z +'+'+ s +'+'+ y +'+'+ x, rt)
+        keyboard.add_hotkey(s +'+'+ z +'+'+ y +'+'+ x, rt)
+        keyboard.add_hotkey(y +'+'+ z +'+'+ x +'+'+ s, rt)
+        keyboard.add_hotkey(y +'+'+ z +'+'+ s +'+'+ x, rt)
+        keyboard.add_hotkey(y +'+'+ s +'+'+ z +'+'+ x, rt)
+        keyboard.add_hotkey(s +'+'+ y +'+'+ z +'+'+ x, rt)
 
-    def fixkey5(x,r,s):
-        keyboard.add_hotkey(x +'+'+ r +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(s+'+'+ x +'+'+ r, on_start_pressed)
-        keyboard.add_hotkey(x +'+'+ s +'+'+ r, on_start_pressed)
-        keyboard.add_hotkey(r +'+'+ x +'+'+ s, on_start_pressed)
-        keyboard.add_hotkey(s +'+'+ r +'+'+ x, on_start_pressed)
-        keyboard.add_hotkey(r +'+'+ s +'+'+ x, on_start_pressed)
+    def fixkey5(x,r,s,rt):
+        keyboard.add_hotkey(x +'+'+ r +'+'+ s, rt)
+        keyboard.add_hotkey(s+'+'+ x +'+'+ r, rt)
+        keyboard.add_hotkey(x +'+'+ s +'+'+ r, rt)
+        keyboard.add_hotkey(r +'+'+ x +'+'+ s, rt)
+        keyboard.add_hotkey(s +'+'+ r +'+'+ x, rt)
+        keyboard.add_hotkey(r +'+'+ s +'+'+ x, rt)
     
-    def mkey(x,y,z,a,r,s):
-        fixkey (x,r)
-        fixkey (y,r)
-        fixkey (a,r)
-        fixkey (z,r)
-        fixkey3 (x,y,r)
-        fixkey3 (x,a,r)
-        fixkey3 (x,z,r)
-        fixkey3 (y,z,r)
-        fixkey3 (y,a,r)
-        fixkey3 (a,z,r)
-        fixkey5 (x,r,s)
-        fixkey5 (y,r,s)
-        fixkey5 (z,r,s)
-        fixkey5 (a,r,s)
-        fixkey4 (x,y,r,s)
-        fixkey4 (x,a,r,s)
-        fixkey4 (x,z,r,s)
-        fixkey4 (y,z,r,s)
-        fixkey4 (y,a,r,s)
-        fixkey4 (a,z,r,s)
-    def tomkey (x,y,z,a,r,s,c,sh):
-        mkey(x,y,z,a,r,s)
-        mkey(x,y,z,c,r,s)
-        mkey(x,y,z,a,r,c)
-        mkey(x,y,z,sh,r,s)
-        mkey(x,y,z,a,r,sh)
-    tomkey ('w','a','s','d','r','space','ctrl','shift')
+    def mkey(x,y,z,a,r,s,rt):
+        fixkey (x,r,rt)
+        fixkey (y,r,rt)
+        fixkey (a,r,rt)
+        fixkey (z,r,rt)
+        fixkey3 (x,y,r,rt)
+        fixkey3 (x,a,r,rt)
+        fixkey3 (x,z,r,rt)
+        fixkey3 (y,z,r,rt)
+        fixkey3 (y,a,r,rt)
+        fixkey3 (a,z,r,rt)
+        fixkey5 (x,r,s,rt)
+        fixkey5 (y,r,s,rt)
+        fixkey5 (z,r,s,rt)
+        fixkey5 (a,r,s,rt)
+        fixkey4 (x,y,r,s,rt)
+        fixkey4 (x,a,r,s,rt)
+        fixkey4 (x,z,r,s,rt)
+        fixkey4 (y,z,r,s,rt)
+        fixkey4 (y,a,r,s,rt)
+        fixkey4 (a,z,r,s,rt)
+    def tomkey (x,y,z,a,r,s,c,sh,rt):
+        mkey(x,y,z,a,r,s,rt)
+        mkey(x,y,z,c,r,s,rt)
+        mkey(x,y,z,a,r,c,rt)
+        mkey(x,y,z,sh,r,s,rt)
+        mkey(x,y,z,a,r,sh,rt)
+    tomkey ('w','a','s','d','r','space','ctrl','shift',on_start_pressed)
+    tomkey ('w','a','s','d','f','space','ctrl','shift',on_start2_pressed)
     keyboard.add_hotkey('r', on_start_pressed)
+    keyboard.add_hotkey('f', on_start2_pressed)
     keyboard.add_hotkey('y', on_stop_pressed)
     keyboard.wait()
 except KeyboardInterrupt:
